9b38b96b559d342cfb2d4eb30fb1fec7067ec78a
kanzz321 <kanzul.ilmi.knz@gmail.com>
2024-08-02 11:23:49 +0700

    terbaru

--- 01-02-Calculations/aritmatika9.py
+++ 01-02-Calculations/aritmatika9.py
@@ -28,10 +28,13 @@ while True:
 
 while True:
     try:
+       # int(input()): Untuk mengonversi input dari string menjadi bilangan bulat sehingga operasi matematika bisa dilakukan.
         operator = input('Pilih operator "penambahan", "pengurangan", "perkalian", atau "pembagian": ').lower()
         number1 = int(input("Masukkan angka pertama: "))
         number2 = int(input("Masukkan angka kedua: "))
-
+ 
+    # if-elif-else: Untuk menentukan operasi matematika yang akan dilakukan berdasarkan input operator.
+       
         if operator == 'penambahan':
             res = number1 + number2
         elif operator == 'pengurangan':
@@ -48,10 +51,20 @@ while True:
     except ValueError:
         res = "Input angka yang dimasukkan tidak valid." 
 
+    #try-except: Untuk menangani kesalahan input yang mungkin terjadi ketika mengonversi input menjadi bilangan bulat.
+
     # Menampilkan hasil
+   
+    #isinstance(res, str): Untuk memeriksa apakah res adalah instance dari tipe str, 
+    # sehingga jika terjadi kesalahan (seperti pembagian dengan nol atau input yang tidak valid), pesan kesalahan bisa dicetak dengan benar.
+
     if isinstance(res, str):
         print(res)
+    
+    #res.is_integer(): Untuk memeriksa apakah hasil operasi matematika adalah bilangan bulat, 
+    # sehingga bisa ditampilkan tanpa desimal jika hasilnya bilangan bulat.
     else:
+ 
         if res.is_integer():
             res = int(res)
         print(f'{operator} dari {number1} dan {number2} adalah {res}')
@@ -60,9 +73,12 @@ while True:
         ulang = input("Apakah kamu mau berhitung lagi? (ya/tidak): ")
 
         if ulang == "tidak":
-            quit("Terimakasih")
+            quit("Terimakasih telah menggunakan aplikasi ini")
         elif ulang == "ya":
             break
+
+    #break: Untuk kembali ke loop utama dan memulai perhitungan baru jika pengguna memilih "ya".
+    # quit(): Untuk keluar dari program jika pengguna memilih "tidak", dengan pesan terima kasih.
         else:
             print("Input tidak valid, silahkan coba lagi.")
             continue
